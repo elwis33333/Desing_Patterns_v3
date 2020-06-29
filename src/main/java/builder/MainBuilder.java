0b1b50532cc836fb77b544cc467c26431375754a
elwis33333 <elwis33333@gmail.com>
2020-06-29 14:27:38 +0200

    builder and command patterns commit

---
+++ src/main/java/builder/MainBuilder.java
@@ -0,0 +1,14 @@
+package main.java.builder;
+
+public class MainBuilder {
+    public static void main(String[] args) {
+        RoomReservation rr =
+                new RoomReservation
+                        .ReservationRoomBuilder(8, 16, 20)
+                        .enableAirCondition()
+                        .isTeleconference()
+                        .build();
+
+        System.out.println(rr);
+    }
+}
